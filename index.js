1ef97c93e8f140173e0fa23c4d1c461c677a1406
IsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-11-04 18:28:34 +0200

    Add error handling to bot environment variables

--- index.js
+++ index.js
@@ -2,11 +2,49 @@ require('dotenv').config();
 
 const mineflayer = require('mineflayer');
 
-const host = process.env.SERVER_HOST || 'localhost';
-const port = parseInt(process.env.SERVER_PORT || '25565', 10);
-const username = process.env.BOT_USERNAME || 'Traducraft';
-const auth = process.env.SERVER_AUTH || 'online';
-const version = process.env.SERVER_VERSION || '1.20.4';
+const host = (() => {
+    if (process.env.SERVER_HOST) {
+        return process.env.SERVER_HOST;
+    }
+    console.error("🛑 Missing server host environment variable. Terminating.");
+    process.exit(1);
+})();
+
+const port = (() => {
+    if (process.env.SERVER_PORT) {
+        const parsedPort = parseInt(process.env.SERVER_PORT, 10);
+
+        if (!isNaN(parsedPort) && parsedPort > 0) {
+            return parsedPort;
+        }
+    }
+    console.warn("⚠️ Missing or invalid server port environment variable. Defaulting to '25565'.")
+    return 25565
+})();
+
+const username = (() => {
+    if (process.env.BOT_USERNAME) {
+        return process.env.BOT_USERNAME;
+    }
+    console.warn("⚠️ Missing bot username environment variable. Defaulting to 'Traducraft'")
+    return 'Traducraft'
+})();
+
+const auth = (() => {
+    if (process.env.SERVER_AUTH) {
+        return process.env.SERVER_AUTH;
+    }
+    console.warn("⚠️ Missing server auth environment variable. Defaulting to 'online'")
+    return 'online'
+})();
+
+const version = (() => {
+    if (process.env.SERVER_VERSION) {
+        return process.env.SERVER_VERSION;
+    }
+    console.error("🛑 Missing server version environment variable. Terminating.");
+    process.exit(1);
+})();
 
 const bot = mineflayer.createBot({
     host: host,
